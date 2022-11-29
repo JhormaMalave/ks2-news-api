0bba5a513836d3f53c3014e52f280234a94748d7
JhormaMalave <jhormamalave99@gmail.com>
2022-11-28 23:01:33 -0400

    Replace promice for async await

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -3,17 +3,21 @@ const db = require("../models");
 const Article = db.Article;
 
 // Retrieve all Articles from the database.
-exports.findAll = (req, res) => {
+exports.findAll = async (req, res) => {
   const title = req.query.title;
 
-  Article.findAll()
-  .then(data => {
-    res.send(data);
-  })
-  .catch(err => {
+  try {
+    const articles = await Article.findAll();
+    res.send(articles);
+  } catch (err) {
     res.status(500).send({
       message:
         err.message || "Ocurrió un error al obtener los artículos."
     });
-  });
+  }
+};
+
+
+exports.create = (req, res) => {
+
 };
